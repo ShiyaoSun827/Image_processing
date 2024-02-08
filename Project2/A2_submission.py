28c1511b890bd61ede2e728cb2cc2424ebc2e3a3
Shiyao Sun <shiyao4@ualberta.ca>
2024-02-07 17:13:04 -0700

    update

---
+++ Project2/A2_submission.py
@@ -0,0 +1,28 @@
+def part1():
+    """add your code here"""
+
+
+def part2():
+    """add your code here"""
+
+
+def part3():
+    """add your code here"""
+
+
+def part4():
+    """add your code here"""
+
+
+def part5():
+    """add your code here"""
+
+if __name__ == '__main__':
+    part1()
+    part2()
+    part3()
+    part4()
+    part5()
+
+
+
